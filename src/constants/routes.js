947ab60b47c7e3b7a46545b748d2dc0aaf5fd847
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2022-11-01 09:50:12 -0600

    [ADD] Favorites Page

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,8 +2,8 @@ const ROUTES = [
   { path: '/', name: 'Inicio' },
   { path: '/pokemons', name: 'Pokedex' },
   { path: '/generations', name: 'Generaciones' },
-  { path: '/favorites', name: 'Favoritos' },
   { path: '/types', name: 'Tipos' },
+  { path: '/favorites', name: 'Favoritos' },
   { path: '/', name: 'Buscar' },
 ];
 
--- src/pages/favorites/index.page.js
+++ src/pages/favorites/index.page.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import PageHeader from '@components/PageHeader';
+import PokemonCard from '@components/PokemonCard';
+import { getDataLocal } from '../../helpers';
+import { FAVORITES_KEY } from '../../constants/data';
 
 function Favorites() {
-  return <div>fdf</div>;
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    setFavorites(getDataLocal(FAVORITES_KEY) || []);
+  }, []);
+
+  return (
+    <section className="container">
+      <PageHeader
+        title="Favoritos"
+        subtitle="Guarda tus pokemones favoritos, visitalos en cualquier momento."
+      />
+      <div
+        className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3
+         lg:grid-cols-4  xl:grid-cols-4  auto-rows-auto gap-7    pb-10"
+      >
+        {favorites.map((favorite) => (
+          <PokemonCard pokemonIdName={favorite} key={favorite} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Favorites;
